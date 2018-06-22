88fe4cbfacc1fe5e826f024a67d4c532af8dfa8d
Mishael Kama <mishealkama@gmail.com>
2018-06-22 13:00:52 +0100

    updated on 22 June, Friday

---
+++ fcaal/addobligor.php
@@ -0,0 +1,322 @@
+
+
+<?php include("header.php"); ?>
+
+
+
+
+
+
+	 <style>
+   	  body {
+   	  	 background: #f9f9f9;
+   	  }
+   </style>
+
+
+				<div class="container first">
+					
+					<div class="row">
+						
+						<div class="col-md-12 calendar">Monday, Apr 28, 2018</div>
+
+						<div class="col-md-12 pageHead">
+							<div class="topic">
+								<i class="material-icons">branding_watermark</i> Manage Projects
+							</div>
+						</div>
+
+					</div>
+
+
+
+					<div class="row">
+						<button class="btn col-md-2 offset-md-10 flowBtn2" style="margin-top: 30px;height: 45px;border-radius:18px;-webkit-border-radius:18px;"><i class="material-icons">cloud_upload</i> Upload Obligors</button>
+						
+						<form class="row">
+
+						<div class="col-md-12 mainPage2">Personal details</div>
+						
+
+
+						<div class="col-md-5">
+							<label for="">Selct an Organisation</label>
+							<select name="" id="" class="form-control">
+								<option value=""></option>
+							</select>
+						</div>
+
+						<div class="col-md-5">
+							<label for="">Choose Project:</label>
+							<select name="" id="" class="form-control">
+								<option value=""></option>
+							</select>
+						</div>
+
+						<div class="col-md-10">
+							<label for="">Obligor Short Name</label>
+							<input type="text" class="form-control">
+						</div>
+
+						<div class="col-md-10">
+							<div class="row">
+								
+								<div class="col-md-4">
+									<label for="">Email address</label>
+									<input type="email" class="form-control">
+								</div>
+
+								<div class="col-md-4">
+									<label for="">PHone Number</label>
+									<input type="text" class="form-control">
+								</div>
+
+								<div class="col-md-4">
+									<label for="">Account Number</label>
+									<input type="text" class="form-control">
+								</div>
+
+								<div class="col-md-4">
+									<label for="">Account Branch Name</label>
+									<input type="text" class="form-control">
+								</div>
+
+								<div class="col-md-4">
+									<label for="">Branch Manager</label>
+									<input type="text" class="form-control">
+								</div>
+
+								<div class="col-md-4">
+									<label for="">Branch Manager's PHone Number</label>
+									<input type="text" class="form-control">
+								</div>
+
+							</div>
+						</div>
+
+
+						<div class="col-md-3">
+							<label for="">Is there any Litigation?</label>
+
+							<div class="row">
+
+								<div class="col-md-12">
+									<label class="containerss">&nbsp; Yes&emsp;&emsp;&emsp;
+							                 <input type="radio" name="disabled">
+							                 <span class="checkmark"></span>
+							             </label>
+
+
+							             <label class="containerss">&nbsp; No
+							                 <input type="radio" name="disabled">
+							                 <span class="checkmark"></span>
+							             </label>
+								</div>
+								
+							</div>
+
+										
+						</div>
+
+
+						<div class="col-md-9"></div>
+
+
+
+						<div class="col-md-12 mainPage2">Loan details</div>
+				
+
+						<div class="col-md-2">
+							<label for="">Select Currency:</label>
+						</div>
+
+						<div class="col-md-2">
+							<select name="" id="" class="form-control">
+								<option value=""></option>
+							</select>
+						</div>
+
+						<div class="col-md-8"></div>
+
+
+						<div class="col-md-3">
+							<label for="">Amount Granted:</label>
+							<input type="text" class="form-control">
+						</div>
+
+						<div class="col-md-3">
+							<label for="">Naira Equivalent:</label>
+							<input type="text" class="form-control">
+						</div>
+
+
+						<div class="col-md-3">
+							<label for="">Facility Type (select):</label>
+							<select name="" id="" class="form-control">
+								<option value=""></option>
+							</select>						
+						</div>
+
+
+						<div class="col-md-6">
+							<label for="">Total Outstanding Amount (Bank Cut-off)</label>
+							<input type="text" class="form-control">
+						</div>
+
+						<div class="col-md-4">
+							<label for="">As at:</label>
+							<div class="row">
+								
+								<div class="col-6">
+									<select name="" id="" class="form-control">
+										<option value="">October</option>
+									</select>
+								</div>
+
+								<div class="col-6">
+									<select name="" id="" class="form-control">
+										<option value="">2018</option>
+									</select>
+								</div>
+							</div>
+						</div>
+
+
+
+						<div class="col-md-6">
+							<label for="">Total Outstanding Amount (When Loan was Purchased)</label>
+							<input type="text" class="form-control">
+						</div>
+
+						<div class="col-md-4">
+							<label for="">As at:</label>
+							<div class="row">
+								
+								<div class="col-6">
+									<select name="" id="" class="form-control">
+										<option value="">October</option>
+									</select>
+								</div>
+
+								<div class="col-6">
+									<select name="" id="" class="form-control">
+										<option value="">2018</option>
+									</select>
+								</div>
+							</div>
+						</div>
+
+
+
+						<div class="col-md-6">
+							<label for="">Total Outstanding Amount (When Obligors were informed)</label>
+							<input type="text" class="form-control">
+						</div>
+
+						<div class="col-md-4">
+							<label for="">As at:</label>
+							<div class="row">
+								
+								<div class="col-6">
+									<select name="" id="" class="form-control">
+										<option value="">October</option>
+									</select>
+								</div>
+
+								<div class="col-6">
+									<select name="" id="" class="form-control">
+										<option value="">2018</option>
+									</select>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="col-md-10">
+							<label for="">Details of Formal Concession Granted</label>
+							<input type="text" class="form-control">
+						</div>
+
+						<div class="col-md-2"></div>
+
+
+					 	<button class="btn offset-md-9">Add Obligor</button>
+
+
+					 </form>
+
+					</div>
+
+
+
+
+
+					<div class="col-md-12 mainPage2">Collaterals</div>
+
+						<form class="col-md-10" action="">
+						
+					<ol>
+
+						
+						<li class="col-md-12">
+							
+							<div class="row">
+								
+								<div class="col-md-12">
+									<label for="">Collateral/Security Details</label>
+									<input type="text" class="form-control">
+								</div>
+
+								<div class="col-md-4">
+									<label for="">Open Market Value of Security</label>
+									<input type="text" class="form-control">
+								</div>
+
+								<div class="col-md-4">
+									<label for="">Forced Sale Value of Security</label>
+									<input type="text" class="form-control">
+								</div>
+
+								<div class="col-md-4">
+									<label for="">Current Status of Security</label>
+									<select name="" id="" class="form-control">
+										<option value=""></option>
+									</select>
+								</div>
+
+								<div class="col-md-12">
+									<label for="">Key additional information / Remark</label>
+									<textarea name="" id="" cols="30" rows="10" class="form-control" style="height: 120px;"></textarea>
+								</div>
+
+							</div>
+
+						</li>
+
+						
+					</ol>
+						
+					<div class="row">
+						<div class="col-md-10"><hr style="width: 100%;background: #4372B8;margin: 0;margin-top: 10px;"></div>
+						<div class="col-md-2"><a href="#" style="font-size: 12px;">+Add more</a></div>
+					</div>
+					
+
+					<button class="btn offset-md-10" disabled>Add Collateral</button>
+				</form>
+
+			</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("footer.php"); ?>
