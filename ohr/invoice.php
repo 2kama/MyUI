b69373ca0b6a59f805dcc5c360ed98ab0eeae6d8
Mishael Kama <mishealkama@gmail.com>
2018-07-03 15:39:50 +0100

    Tuesday, 3rd July

---
+++ ohr/invoice.php
@@ -0,0 +1,107 @@
+<?php include('header.php'); ?>
+
+
+
+
+
+    <div class="container" style="margin-top: 130px;margin-bottom: 70px;">
+    	
+  			<div class="row case">
+  				
+                   <div class="col-md-5">
+                       <div class="topHead">Item</div>
+                       <div class="bottomHead">confirmation of employment</div>
+                   </div>
+                   <div class="col">
+                        <div class="topHead">Price</div>
+                       <div class="bottomHead">NGN 4000.00</div>
+                   </div>
+                   <div class="col">
+                        <div class="topHead">Quantity</div>
+                       <div class="bottomHead">1</div>
+                   </div>
+                   <div class="col">
+                         <div class="topHead">Item</div>
+                         <div class="bottomHead">NGN 4000.00</div>
+                   </div>
+
+              </div>
+              
+              <div class="row case" style="border-top:none;">
+
+                    <div class="col-md-4 offset-md-8">
+                        <div class="row">
+                            <div class="col-6" style="text-align:right;">
+                                <p>Sub total</p>
+                                <p>Sales Tax</p>
+                                <p>Shipping charge</p>
+                                <p>Total amount</p>
+                            </div>
+                            <div class="col-6" style="color:#5e5e5e;">
+                                <p>NGN 4000.00</p>
+                                <p>NGN 0.00</p>
+                                <p>NGN 0.00</p>
+                                <p style="color:#707bea;font-weight:600;">NGN 4000.00</p>
+                            </div>
+                        </div>
+                    </div>
+
+              </div>
+
+
+               <div class="row case" style="border-top:none;">
+
+                <div class="col-md-4 offset-md-8" style="padding-top:50px;padding-bottom:50px;">
+                    <div class="row">
+                        <button class="btn col-3" style="background:#fff;color:#575a5d;">BACK</button>
+                        <button class="btn col-7" style="background:#707bea;color:#fff;">DOWNLOAD DOCUMENT</button>
+                    </div>
+                </div>
+
+                </div>
+
+    </div>
+	
+
+
+<style>
+    .case button {
+        font-size: 11px;
+        border-radius: 3px;
+        -webkit-border-radius: 3px;
+        height: 40px;
+    }
+    .col-6 {
+        font-size: 14px;
+        padding: 50px 10px;
+    }
+    body {
+        background: #f7f8fa;
+    }
+    .case {
+        color: #9b9b9b;
+        background: #fff;
+        border: 1px solid #bac3ca;
+        
+        padding: 0;
+    }
+    .case .col-md-5, .case .col {
+        padding: 0;
+        margin: 0;
+    }
+    .case .topHead {
+        background: #f5f5f5;
+
+    }
+    .bottomHead {
+        border-bottom: 1px solid #bac3ca;
+    }
+    .topHead, .bottomHead {
+        padding: 30px;
+        font-size: 14px;
+    }
+</style>
+
+
+
+<?php include('footer.php'); ?>
