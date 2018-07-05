85d526a5af201640121821ff40d5fbab69bb9b0b
Mishael Kama <mishealkama@gmail.com>
2018-07-05 10:22:41 +0100

    Thurday, 5th July - second badge

---
+++ fcaal/bibledetail.php
@@ -0,0 +1,237 @@
+<?php include("header.php"); ?>
+
+
+
+    <style>
+   	  body {
+   	  	 background: #f9f9f9;
+   	  }
+   </style>
+
+
+				<div class="container first">
+					
+					<div class="row">
+						
+						<div class="col-md-12 calendar">Monday, Apr 28, 2018</div>
+
+						<div class="col-md-12 pageHead">
+							<div class="topic">
+								<i class="material-icons">branding_watermark</i> Manage Projects
+							</div>
+						</div>
+
+					</div>
+
+
+
+
+                    <div class="row">
+                    
+                                <div class="col-md-12" style="padding-top: 25px;">
+
+                                    <div class="row">
+
+                                        <div class="col-md-3">
+                                            <div class="row">
+                                                <div class="col-4"><label for="">Filter by:</label></div>
+
+                                                <div class="col-8">
+                                                    <select name="" id="" class="form-control curve">
+                                                        <option value="">Category</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-2">
+                                            <select name="" id="" class="form-control curve">
+                                                    <option value="">Loan Range</option>
+                                             </select>
+                                        </div>
+
+                                        <div class="col-md-3 offset-md-1 sam">
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <select name="" id="" class="form-control sad">
+                                                        <option value="">Export to</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col-6">
+                                                    <i class="material-icons">print</i>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-2 offset-md-1">
+                                            <select name="" id="" class="form-control curve">
+                                                    <option value="">Sort by Location</option>
+                                             </select>
+                                        </div>
+                                        
+                                        
+
+                                    </div>
+
+
+                                </div>
+
+
+                                <div class="tableRow" style="margin-top:20px;">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <td>Customer Short Name</td>
+                                                <td>Total outstanding balance OCTOBER 2016</td>
+                                                <td>Total outstanding balance JULY 2017</td>
+                                                <td>Collateral Categorisation</td>
+                                                <td>Implied Price/Price Payable</td>
+                                                <td></td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+
+                                                <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>FERTILIZER & CHEMICAL LTD</td>
+                                                <td>13,490,255,865.41</td>
+                                                <td>14,641,758,523.64</td>
+                                                <td>B</td>
+                                                <td>674,512,793.27</td>
+                                                <td><button class="btn flowBtn">View details</button></td>
+                                            </tr>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                    
+                    </div>
+
+
+
+
+                    <nav aria-label="Page navigation example" class="no-nav">
+						  <ul class="pagination justify-content-end">
+						    <li class="page-item"> <a class="page-link" href="#">&lt;</a></li>
+						    <li class="page-item"><a class="page-link" href="#">1</a></li>
+						    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+						    <li class="page-item"><a class="page-link" href="#">3</a></li>
+						    <li class="page-item"><a class="page-link" href="#">4</a></li>
+						    <li class="page-item"><a class="page-link" href="#">5</a></li>
+						    <li class="page-item disabled"><a class="page-link" href="#">...</a></li>
+						    <li class="page-item"><a class="page-link" href="#">50</a></li>
+						    <li class="page-item"><a class="page-link" href="#">&gt;</a></li>
+
+						    <li class="page-item">
+						    	<select name="" id="" class="form-control">
+						    		<option value="">10p </option>
+						    		<option value="">50p</option>
+						    		<option value="">100p</option>
+						    	</select>
+						    </li>
+						    
+						  </ul>
+						</nav>
+
+
+
+
+
+
+
+                </div>
+
+
+
+
+
+
+
+<?php include("footer.php"); ?>
