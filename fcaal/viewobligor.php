b2c369cec710ee200ce45cc7ec3a5f974473a75e
Mishael Kama <mishealkama@gmail.com>
2018-07-06 15:37:41 +0100

    Friday, 6th July - New Update

---
+++ fcaal/viewobligor.php
@@ -0,0 +1,263 @@
+<?php include("header.php"); ?>
+
+
+
+
+
+
+
+    
+	 <style>
+   	  body {
+   	  	 background: #f9f9f9;
+   	  }
+   </style>
+
+
+				<div class="container first">
+
+					
+					<div class="row">
+						
+						<div class="col-md-12 calendar">Monday, Apr 28, 2018</div>
+
+						<div class="col-md-12 pageHead">
+							<div class="topic">
+								<i class="fa fa-user"></i> Manage Obligors
+							</div>
+						</div>
+
+                    </div>
+
+
+                </div>
+
+
+                <div class="container blueTop">
+                    
+
+                    <div class="row">
+                        <div class="col-md-9">
+                            <div class="row">
+
+                                 <div class="col-md-10">GIANT STRIDES PROP&DEV. COY LT</div>
+                                 <div class="col-md-2"><i class="material-icons">room</i> Lagos</div>
+
+                                 <div class="col-md-4">Account number:</div>
+                                 <div class="col-md-6">233/1301957/1/2/0</div>
+                                 <div class="col-md-2"></div>
+
+                                 <div class="col-md-4">Region:</div>
+                                 <div class="col-md-6">Lagos & West</div>
+                                 <div class="col-md-2"></div>
+
+                                 <div class="col-md-4">Account branch name:</div>
+                                 <div class="col-md-6">Adeola Odeku Branch</div>
+                                 <div class="col-md-2"></div>
+
+                                 <div class="col-md-4">Phone number:</div>
+                                 <div class="col-md-6">08023554475</div>
+                                 <div class="col-md-2"><i class="material-icons">edit</i> Edit</div>
+
+                                 <div class="col-md-4">Address:</div>
+                                 <div class="col-md-6">11, Akin Adesola Str., Ogba, Lagos</div>
+                                 <div class="col-md-2"><i class="material-icons">edit</i> Edit</div>
+
+                                 <div class="col-md-4">Manager's name:</div>
+                                 <div class="col-md-6">Odu Nyire Vera Mrs.</div>
+                                 <div class="col-md-2"></div>
+
+                                 <div class="col-md-4">Manager's Phone number:</div>
+                                 <div class="col-md-6">07034253553</div>
+                                 <div class="col-md-2"><i class="material-icons">edit</i> Edit</div>
+
+                                 <div class="col-md-4">Recovery Agent:</div>
+                                 <div class="col-md-6">N/A</div>
+                                 <div class="col-md-2"></div>
+
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <a class="btn btnGreenInverse btn-block">Assign DRA</a>
+
+                            <a class="btn btnGreen btn-block">Contact DRA</a>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+
+
+                <div class="container mishTab">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="list-group" id="list-tab" role="tablist">
+                            <a class="list-group-item list-group-item-action active" id="list-loan-list" data-toggle="list" href="#list-loan" role="tab" aria-controls="loan">Loan Details</a>
+                            <a class="list-group-item list-group-item-action" id="list-bank-list" data-toggle="list" href="#list-bank" role="tab" aria-controls="bank">Bank Statement</a>
+                            <a class="list-group-item list-group-item-action" id="list-correspond-list" data-toggle="list" href="#list-correspond" role="tab" aria-controls="correspond">Correspondence</a>
+                            <a class="list-group-item list-group-item-action" id="list-collateral-list" data-toggle="list" href="#list-collateral" role="tab" aria-controls="collateral">Collateral</a>
+                            <a class="list-group-item list-group-item-action" id="list-docu-list" data-toggle="list" href="#list-docu" role="tab" aria-controls="docu">Documents</a>
+                            <a class="list-group-item list-group-item-action" id="list-addition-list" data-toggle="list" href="#list-addition" role="tab" aria-controls="addition">Additional remarks</a>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="tab-content" id="nav-tabContent">
+
+                            <div class="tab-pane fade show active" id="list-loan" role="tabpanel" aria-labelledby="list-loan-list">
+                                <div class="row">
+
+                                    <div class="col-md-4 royalBlue">
+                                        <div class="handHead">Amount Granted</div>
+                                        <div class="handBody">
+                                            <p class="bigBody">₦ 5,997,765,375.00</p>
+                                            <p class="smallBody">Naira Equivalent:<br>--</p>
+                                        </div>
+
+                                        <div class="handHead">Total Outstanding balance</div>
+                                        <div class="handBody">
+                                            <p class="smallBody">
+                                                <select name="" id="">
+                                                    <option value="">October</option>
+                                                </select>
+
+                                                <select name="" id="">
+                                                    <option value="">2018</option>
+                                                </select>
+                                            </p>
+                                            <p class="bigBody">₦ 9,503,778,851.97</p>
+                                            
+                                        </div>
+
+                                        <div class="handHead">Total Outstanding balance (Current)</div>
+                                        <div class="handBody">
+                                            <p class="smallBody">June 2018</p>
+                                            <p class="bigBody">₦ 9,503,778,851.97</p>
+                                        </div>
+
+
+                                    </div>
+
+                                    
+
+                                    <div class="col-md-4 sky">
+                                    <div class="handHead">Total Collected</div>
+                                        <div class="handBody">
+                                        <p class="smallBody">Between October 2016 and July 2017</p>
+                                            <p class="bigBody">₦ 5,997,765,375.00</p>
+                                            
+                                        </div>
+
+                                        <div class="handHead">Total Outstanding balance</div>
+                                        <div class="handBody">
+                                            <p class="smallBody">June 2017</p>
+                                            <p class="bigBody">₦ 5,997,765,375.00</p>
+                                            
+                                        </div>
+
+                                        <div class="handHead">Total Outstanding balance (Current)</div>
+                                        <div class="handBody">
+                                            <p class="smallBody">
+                                                <select name="" id="">
+                                                    <option value="">October</option>
+                                                </select>
+
+                                                <select name="" id="">
+                                                    <option value="">2018</option>
+                                                </select>
+                                            </p>
+                                            <p class="bigBody">₦ 5,997,765,375.00</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-4 orange">
+
+                                        <div class="handHead">Next payment</div>
+                                        <div class="handBody">
+                                            <p class="smallBody">Amount</p>
+                                            <p class="bigBody">₦ 20,000,000</p>
+
+                                            <p class="smallBody" style="margin-top:10px;">Due Date:</p>
+                                            <p class="bigBody">May 13, 2018</p>
+
+                                            <button class="btn">Send a reminder</button>
+                                            
+                                        </div>
+
+
+                                        <div class="handHead">Other Charges</div>
+                                        <div class="handBody">
+                                            <p class="smallBody">Between October 2016 and July 2017</p>
+                                            <p class="bigBody">--</p>
+                                            
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="tab-pane fade" id="list-bank" role="tabpanel" aria-labelledby="list-bank-list">...</div>
+                            <div class="tab-pane fade" id="list-correspond" role="tabpanel" aria-labelledby="list-correspond-list">...</div>
+
+                            <div class="tab-pane fade" id="list-collateral" role="tabpanel" aria-labelledby="list-collateral-list">
+
+                                 <div class="row">
+                                     <div class="col-md-12 superGreen">
+                                        <div class="handHead">Collateral/Security Details</div>
+                                        <div class="handBody">
+                                            <p class="smallBody">Legal Mortgage over proeprty located at plot 1760 jimmy carter street asokoro, abuja c of o FCT/ABU/PL/323 valued at 975M FSV.</p>
+                                            <p class="smallBody">LM over property located at plot 909 cadesr=tral zone AO3 Aguta close area 11 Garki Abuja covered with c of o No. FCT/ABU/CR-586 valued at N675M FSV.</p>
+                                            
+                                        </div>
+                                     </div>
+                                 </div>
+
+                                 <div class="row">
+                                     <div class="col-md-4 water">
+                                         <div class="handHead">Open Market Value of Security</div>
+                                         <div class="handBody">
+                                             <p class="bigBody">₦ 5,997,765,375.00</p>
+                                         </div>
+                                     </div>
+
+                                     <div class="col-md-4 water">
+                                         <div class="handHead">Forced Sale Value of Security</div>
+                                         <div class="handBody">
+                                             <p class="bigBody">₦ 3,685,000,000.00</p>
+                                         </div>
+                                     </div>
+
+                                     <div class="col-md-4 water">
+                                         <div class="handHead">Current Status of Collateral</div>
+                                         <div class="handBody">
+                                             <p class="bigBody">A</p>
+                                         </div>
+                                     </div>
+                                 </div>
+
+                            </div>
+
+                            <div class="tab-pane fade" id="list-docu" role="tabpanel" aria-labelledby="list-docu-list">...</div>
+                            <div class="tab-pane fade" id="list-addition" role="tabpanel" aria-labelledby="list-addition-list">...</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("footer.php"); ?>
