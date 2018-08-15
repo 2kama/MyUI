51fe71444c8dde89690791399243e15f3a660c5a
Mishael Kama <mishealkama@gmail.com>
2018-08-15 15:51:06 +0100

    Kunle (Aug 15 - Wed)

---
+++ fcaal/legal_incoming.php
@@ -0,0 +1,173 @@
+<?php include("header.php"); ?>
+
+
+
+
+                <style>
+					body {
+						background: #f9f9f9;
+					}
+				</style>
+
+				<div class="container first">
+					
+					<div class="row">
+						
+						<div class="col-md-12 calendar">Monday, Apr 28, 2018</div>
+
+						<div class="col-md-12 pageHead">
+							<div class="topic">
+								<i class="fa fa-legal"></i> Manage Litigation 
+							</div>
+						</div>
+
+					</div>
+
+
+
+					<div class="row">
+						
+ 
+						<div class="col-md-12" style="padding-top: 25px;">
+
+							<div class="row">
+								
+								<div class="col-md-5">
+                                    <input type="text" placeholder="Search by Obligor Name" class="form-control curve">
+                                </div>
+
+
+							</div>
+							
+							
+						</div>
+					</div>
+
+
+
+
+
+					<div class="row" style="padding-top: 30px;">
+						
+						<div class="tableRow">
+							
+						
+							<table class="table table-striped">
+								<thead>
+									<tr>
+										<th>Obligor Name</th>
+										<th>Total Outstanding<br>  Bal. October 2016</th>
+										<th>Total Outstanding  <br>Bal. July 2017</th>
+                                        <th>Collateral Class</th>
+                                        <th>Implied Price</th>
+                                        <th>Action</th>
+									</tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><a href="manage_litigation"><button class="btn flowBtn2">Add Litigation details</button></a></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MAIWAKE YUSUF  BARAU</td>
+                                        <td> 13,490,255,865.41 </td>
+                                        <td> 14,641,758,523.64 </td>
+                                        <td>A</td>
+                                        <td> 674,512,793.27 </td>
+                                        <td><button class="btn flowBtn2">Add Litigation details</button></td>
+                                    </tr>
+                                </tbody>
+                                
+
+                            </table>
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+                    </div>
+
+
+
+
+
+<?php include("footer.php"); ?>
---
+++ fcaal/manage_litigation.php
@@ -0,0 +1,151 @@
+<?php include("header.php"); ?>
+
+
+
+
+                <style>
+					body {
+						background: #f9f9f9;
+                    }
+                    .form-control {
+                        margin-bottom: 10px;
+                    }
+				</style>
+
+				<div class="container first">
+					
+					<div class="row">
+						
+						<div class="col-md-12 calendar">Monday, Apr 28, 2018</div>
+
+						<div class="col-md-12 pageHead">
+							<div class="topic">
+								<i class="fa fa-legal"></i> Manage Litigation 
+                            </div>
+                            
+                            <button class="btn goback" style="margin-top: 20px;">GO BACK</button>
+						</div>
+
+					</div>
+
+
+
+
+					<div class="row sam1 col-md-9" style="padding-top: 30px;">
+					   
+                        <div class="col-md-5">Obligor Name</div>
+                        <div class="col-md-7">MAIWAKE YUSUF  BARAU1</div>
+                        <div class="col-md-5">Account Number</div>
+                        <div class="col-md-7"> 233/1301957/1/2/0 </div>
+                        <div class="col-md-5">State</div>
+                        <div class="col-md-7">Lagos</div>
+                        <div class="col-md-5">Court</div>
+                        <div class="col-md-7">---</div>
+                                    
+
+                    </div>
+
+
+                    <form class="row col-md-9">
+                        <div class="col-md-12"><label class="main" for="">Litigation Details</label></div>
+
+                        <div class="col-md-3">
+                            <label for="">Status</label>
+                        </div>
+                        <div class="col-md-4">
+                            <select name="" id="" class="form-control">
+                                <option value="">Pending</option>
+                            </select>
+                        </div>
+                        <div class="col-md-5"></div>
+
+                        <div class="col-md-3">
+                            <label for="">Court</label>
+                        </div>
+                        <div class="col-md-4">
+                            <select name="" id="" class="form-control">
+                                <option value="">Appeal Court</option>
+                            </select>
+                        </div>
+                        <div class="col-md-5"></div>
+
+                        <div class="col-md-12"><label for="" class="main">Details of Counsel</label></div>
+
+                        <div class="col-md-3">
+                            <label for="">Full Name</label>
+                        </div>
+                        <div class="col-md-8">
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="">Email Address</label>
+                        </div>
+                        <div class="col-md-8">
+                            <input type="email" class="form-control">
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="">Phone Number 1</label>
+                        </div>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="">Phone Number 2</label>
+                        </div>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="">Contact Address</label>
+                        </div>
+                        <div class="col-md-8">
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="">Website (if any)</label>
+                        </div>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="col-md-12"><label for="" class="main">Litigation UPdate</label></div>
+
+                        <div class="col-md-11">
+                            <textarea name="" style="height:100px;" id="" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+
+                        <div class="col-md-3 offset-md-8">
+                            <label class="containers text-danger"> Mark as urgent
+                                <input type="checkbox">
+                                <span class="checkmark"></span>
+                            </label>
+                        </div>
+
+                        <div class="col-md-3 offset-md-8">
+                            <button class="btn" style="margin-top:10px;width:100%;">Add</button>
+                        </div>
+
+
+                    </form>
+
+
+
+
+
+
+
+
+
+
+
+                    </div>
+
+
+
+
+<?php include("footer.php"); ?>
