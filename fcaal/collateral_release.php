25ed91866aa9c1eef81ba2f3bee0ccc10e80c01f
Mishael Kama <mishealkama@gmail.com>
2018-10-10 23:15:57 +0100

    New collateral Release and main.css

---
+++ fcaal/collateral_release.php
@@ -0,0 +1,173 @@
+<?php include('header.php'); ?>
+   <style>
+   	  body {
+   	  	 background: #f9f9f9;
+   	  }
+   </style>
+
+
+
+				<div class="container first">
+					
+                            <div class="row">
+                                
+                                <div class="col-md-12 calendar">Monday, Apr 28, 2018</div>
+
+                                <div class="col-md-12 pageHead">
+                                    <div class="topic">
+                                        <i class="fa fa-archive"></i> Manage Collateral
+                                    </div>
+
+                                    
+                                </div>
+
+                            </div>
+
+
+
+                 </div>
+
+
+
+
+
+                 
+
+                <div class="container mishTab">
+                    <div class="row">
+
+                            <div class="col-md-12">
+                                <p class="text-primary">Upload supporting documents for Collateral release</p>
+                            </div>
+
+
+                    </div>
+
+                    <form action="" class="row">
+
+                            <div class="col-md-7">
+
+                                    <label for="">Select Document type</label>
+                                    <select name="" id="" class="form-control">
+                                        <option value=""></option>
+                                        <option value=""></option>
+                                        <option value=""></option>
+                                        <option value=""></option>
+                                    </select>
+
+                                    <label for="">Document Name</label>
+                                    <input type="text" class="form-control">
+
+                                    <label for="">Description</label>
+                                    <textarea name="" style="height:70px;" class="form-control"></textarea>
+
+                            </div>
+
+                            <div class="col-md-5" style="padding: 0px 50px;">
+
+                                  <div class="uploadBox2 row bg-white text-center" style="border:1px dashed #aaa">
+                                        <i class="fa fa-cloud-upload text-center"></i>
+
+                                        <p class="text-center">
+                                        Drag and Drop files here to upload <br>
+                                        or
+                                        </p>
+                                        <div class="col-md-4"></div>
+                                        <button class="btn btn-primary col-md-4">Browse</button>
+                                  </div>
+
+                            </div>
+
+
+                            <div class="col-md-12">
+                                <button class="btn col-md-4 offset-md-4">SAVE</button>
+                            </div>
+
+                    </form>
+
+
+
+                    <div class="col-md-12 headss">
+                        <p class="text-primary">Uploaded Documents</p>
+                    </div>
+
+                    <div class="row">
+                        
+                        <!--upload item-->
+                        <div class="col-md-12 uploadsBlock">
+                             <div class="row">
+                                 <div class="col-md-4 col-sm-4">
+                                     <span>Instruction letter to Unity Bank</span>
+                                     <sup>Allas</sup>
+                                 </div>
+
+                                 <div class="col-md-6 col-sm-6">
+                                     <p>
+                                     Instruction was sent to the bank for collateral release.
+                                     </p>
+                                 </div>
+                                 <div class="col-md-2 col-sm-2">
+                                     <i class="fa fa-eye text-primary"></i>
+                                     <i class="fa fa-edit text-info"></i>
+                                     <i class="fa fa-times text-danger"></i>
+                                 </div>
+                             </div>
+                        </div>
+                        <!--end upload item -->
+
+                        <!--upload item-->
+                        <div class="col-md-12 uploadsBlock">
+                             <div class="row">
+                                 <div class="col-md-4 col-sm-4">
+                                     <span>Instruction letter to Unity Bank</span>
+                                     <sup>Allas</sup>
+                                 </div>
+
+                                 <div class="col-md-6 col-sm-6">
+                                     <p>
+                                     Instruction was sent to the bank for collateral release.
+                                     </p>
+                                 </div>
+                                 <div class="col-md-2 col-sm-2">
+                                     <i class="fa fa-eye text-primary"></i>
+                                     <i class="fa fa-edit text-info"></i>
+                                     <i class="fa fa-times text-danger"></i>
+                                 </div>
+                             </div>
+                        </div>
+                        <!--end upload item -->
+
+                        <!--upload item-->
+                        <div class="col-md-12 uploadsBlock">
+                             <div class="row">
+                                 <div class="col-md-4 col-sm-4">
+                                     <span>Instruction letter to Unity Bank</span>
+                                     <sup>Allas</sup>
+                                 </div>
+
+                                 <div class="col-md-6 col-sm-6">
+                                     <p>
+                                     Instruction was sent to the bank for collateral release.
+                                     </p>
+                                 </div>
+                                 <div class="col-md-2 col-sm-2">
+                                     <i class="fa fa-eye text-primary"></i>
+                                     <i class="fa fa-edit text-info"></i>
+                                     <i class="fa fa-times text-danger"></i>
+                                 </div>
+                             </div>
+                        </div>
+                        <!--end upload item -->
+
+                    </div>
+
+
+
+                </div>
+
+
+
+
+<?php include('footer.php'); ?>
+
+
